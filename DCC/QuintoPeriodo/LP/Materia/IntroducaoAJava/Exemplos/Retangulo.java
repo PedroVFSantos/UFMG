4c55085c4706c653cbe50526c6f098b3bb3fdecc
PedroVFSantos <pedro.vinicius298@gmail.com>
2023-10-20 18:54:37 -0300

    atualizando pelo notebook

---
+++ DCC/QuintoPeriodo/LP/Materia/IntroducaoAJava/Exemplos/Retangulo.java
@@ -0,0 +1,18 @@
+package DCC.QuintoPeriodo.LP.Materia.IntroducaoAJava.Exemplos;
+
+/**
+ * Retangulo
+ */
+public class Retangulo {
+
+  private float lado;
+  private float altura;
+}
+
+public Retangulo(float lado, float altura){
+  lado = ld;
+  altura = alt;
+}
+
+public float getLado() {return lado;}
+public float getArea() {return altura;}
